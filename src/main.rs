94fff4199242e7c983e8a037fc1645de06e111ac
Stein Somers <git@steinsomers.be>
2021-07-06 14:13:42 +0200

    add range

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,10 @@ fn main() {
                 let mut min_leading = Sample::BITS;
                 let mut min_trailing0 = Sample::BITS;
                 let mut min_trailing1 = Sample::BITS;
-                let mut samples = 0usize;
+                let mut max_value = 0;
+                let mut min_value = 0;
                 let mut values = BTreeSet::<Sample>::new();
+                let mut samples = 0usize;
                 for sample in stream.iter::<Sample>() {
                     let leading = if sample.is_negative() {
                         sample.leading_ones()
@@ -29,6 +31,8 @@ fn main() {
                     min_leading = min_leading.min(leading);
                     min_trailing0 = min_trailing0.min(sample.trailing_zeros());
                     min_trailing1 = min_trailing1.min(sample.trailing_ones());
+                    max_value = max_value.max(sample);
+                    min_value = min_value.min(sample);
                     values.insert(sample);
                     samples += 1;
                 }
@@ -40,6 +44,16 @@ fn main() {
                 println!("expected samples: {}", expected.to_formatted_string(locale));
                 println!("streamed samples: {}", samples.to_formatted_string(locale));
                 println!("distinct samples: {}", distinct.to_formatted_string(locale));
+                println!(
+                    "possible sample range: {} … {}",
+                    (-1 << (info.bits_per_sample - 1)).to_formatted_string(locale),
+                    (1 << (info.bits_per_sample - 1)).to_formatted_string(locale)
+                );
+                println!(
+                    "streamed sample range: {} … {}",
+                    min_value.to_formatted_string(locale),
+                    max_value.to_formatted_string(locale)
+                );
             }
             Err(error) => println!("{}: error \"{:?}\"", fname, error),
         }
