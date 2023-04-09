09245818e4dac50aa198327bb0b89fe0fef99af8
Stein Somers <git@steinsomers.be>
2023-04-09 20:31:53 +0200

    Update & cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,22 @@ use std::collections::BTreeSet;
 use std::env;
 use std::fs::File;
 
+/* How many significant bits?
+    no samples: 0
+    all 0: 0 or 1
+    all 1: 0 or 1
+    some 0, some 1: 1
+    some 0x4000, some 0: 31
+    some 0x8000, some 0: 31
+    some 0x7FFF, some 0x8001: 32
+*/
+
 fn main() {
     type Sample = i32;
     let locale = &Locale::en;
-    let args: Vec<String> = env::args().collect();
 
-    for fname in &args[1..] {
-        match StreamReader::<File>::from_file(fname) {
+    for fname in env::args().skip(1) {
+        match StreamReader::<File>::from_file(fname.as_str()) {
             Ok(mut stream) => {
                 let info = stream.info();
                 println!("channels: {}", info.channels);
@@ -24,9 +33,9 @@ fn main() {
                 let mut samples = 0usize;
                 for sample in stream.iter::<Sample>() {
                     let leading = if sample.is_negative() {
-                        sample.leading_ones()
+                        sample.leading_ones() - 1
                     } else {
-                        sample.leading_zeros()
+                        sample.leading_zeros() - 1
                     };
                     min_leading = min_leading.min(leading);
                     min_trailing0 = min_trailing0.min(sample.trailing_zeros());
@@ -38,7 +47,7 @@ fn main() {
                 }
                 let expected = info.total_samples * u64::from(info.channels);
                 let distinct = values.len();
-                println!("significant: {}", Sample::BITS + 1 - min_leading);
+                println!("significant: {}", Sample::BITS - min_leading);
                 println!("trailing 0s: {}", min_trailing0);
                 println!("trailing 1s: {}", min_trailing1);
                 println!("expected samples: {}", expected.to_formatted_string(locale));
